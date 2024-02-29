4d634b5612ac8c5c437b55f454e40d21ff20bfb6
inareu <39516834+inareu@users.noreply.github.com>
2024-02-29 14:11:40 +0000

    29.02.2024 Lexer - started with Tokenizer

--- lexer.c
+++ lexer.c
@@ -31,6 +31,51 @@ int check_type_tk(char *token)
     *have to be more efficient tokenizing the input, bc is not working on somes cases, like "ls|wc"
 */
 
+t_token *split_input1(char *input)
+{
+    t_token *tokens;
+    t_token *start;
+
+    tokens = malloc(sizeof(t_token));
+    start = tokens;
+    fill_tokens(tokens, input);
+}
+
+void    fill_token(t_token *tokens, char *input)
+{
+    char    *text;
+    int     i;
+    int     t;
+
+    i = 0;
+    t = 0;
+    while (input[i])
+    {
+        if (input[i] == ' ')
+        {
+            i++;
+            if (input[i] == 34)
+            {
+                i++;
+                while (input[i] && input[i] != 34)
+                {
+                    t++;
+                    i++;
+                }
+                if (!input[i])
+                {
+                    free_tokens(tokens);
+                    error("some error message", 2); // idea for an error function: give it the error message it should print out & a code (int), which describes what we have to free (e.g.: 1 = free the very first malloc we created, 2 = free the first 2 mallocs)
+                }
+                
+            }
+            if (input[i] == 39)
+            check_delimiter
+
+        }
+    }
+}
+
 t_token *split_input(char *input)
 {
     t_token *tokens;
