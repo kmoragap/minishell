83d6b668f3089439311cb526b8ced818e47004e9
Kris Moraga Poblete <kris.moraga.p@gmail.com>
2024-05-26 19:35:08 +0200

    funcheck stuff

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -6,7 +6,7 @@
 /*   By: kmoraga <kmoraga@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:43:08 by creuther          #+#    #+#             */
-/*   Updated: 2024/05/19 12:58:30 by kmoraga          ###   ########.fr       */
+/*   Updated: 2024/05/26 19:34:51 by kmoraga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ void	input_error(t_data *data, t_free code, int exit_code, char *txt)
 
 void	malloc_error(t_data *data, t_free code)
 {
-	data->free_code = code;
-	data->err_code = ER_MALLOC;
-	data->exit_code = 12;
+	if (data)
+	{
+		data->free_code = code;
+		data->err_code = ER_MALLOC;
+		data->exit_code = 12;
+	}
 	write(2, "Error: malloc failure\n", 22);
 }
